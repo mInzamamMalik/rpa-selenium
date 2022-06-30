fa2647c5f449d7e0260d2e32476b38c6588812ce
M.Inzamam Malik <malikasinger1@gmail.com>
2022-06-30 19:57:28 +0500

    working on youtube

--- app.py
+++ app.py
@@ -1,23 +1,48 @@
+from random import random
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import geckodriver_autoinstaller
-from selenium.webdriver.common.keys import Keys
 import time
 
 geckodriver_autoinstaller.install()
-driver = webdriver.Firefox()
+# driver = webdriver.Firefox()
+
+firefox_profile = webdriver.FirefoxProfile()
+
+# allow video autoplay
+firefox_profile.set_preference('media.autoplay.default', 1)
+firefox_profile.set_preference('media.autoplay.allow-muted', False)
+driver = webdriver.Firefox(firefox_profile=firefox_profile)
+
 wait = WebDriverWait(driver, 10)
 
-driver.get('https://www.youtube.com/watch?v=sFS0vmV54vY')
+playButtonXpath = '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[26]/div[2]/div[1]/button'
+videoUrl = 'https://www.youtube.com/watch?v=sFS0vmV54vY'
+videoLengthInSeconds = 30
 
-time.sleep(6)
+driver.get(videoUrl)
+time.sleep(10 + (random() * 10))
+# wait.until(EC.presence_of_element_located((By.XPATH, playButtonXpath)))
 
-id_input = driver.find_element(
-    By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[26]/div[2]/div[1]/button')
-id_input.click()
+while True:
 
-time.sleep(30)
+    playButton = driver.find_element(By.XPATH, playButtonXpath)
+    # try:
+    #     playButton = WebDriverWait(driver, 10).until(
+    #         EC.presence_of_element_located((By.XPATH, playButtonXpath))
+    #     )
+    # finally:
+    #     driver.quit()
 
-driver.quit()
+    playButton.click()
+
+    time.sleep(random() * videoLengthInSeconds)
+
+    driver.refresh()
+    time.sleep(5 + (random()+5))
+    # wait.until(EC.presence_of_element_located((By.XPATH, playButtonXpath)))
+
+    # driver.quit()
